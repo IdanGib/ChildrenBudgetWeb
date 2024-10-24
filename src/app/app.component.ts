407c775c76dea490be2d9733912d489a27f805e8
Idan <idangibly.dev@gmail.com>
2024-10-24 18:14:54 +0300

    setup material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ChildrenBudgetWeb';
+  title = 'Children Budget';
 }
