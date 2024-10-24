dad13431398f6c1490f9d94a7e1308bea2672f3a
Idan <idangibly.dev@gmail.com>
2024-10-24 18:36:18 +0300

    tes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatDividerModule],
+  imports: [RouterOutlet, MatDividerModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Children Budget';
+  title = '';
 }
